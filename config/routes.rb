fb11167660534035e445048509b5ff944e7aba10
Liang Xinlei <xinlei.liang@whalemove.com>
2018-02-01 15:26:11 +0800

    Grape antoreload change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  require 'dispatch'
-  mount Api::Dispatch => '/'
+
+  api_path = Rails.root.join('app', 'api').to_s
+  Dir.foreach(api_path) do |version|
+    next if ['.', '..'].include? version
+    version_path = api_path + '/' + version
+    if File.directory?(version_path)
+      Dir.foreach(version_path) do |resource_name|
+        next if ['.', '..'].include? resource_name
+        resource_path = version_path + '/' + resource_name
+        if File.directory?(resource_path)
+          Dir.foreach(resource_path) do |action_file|
+            if action_file[-10, 10] == '_action.rb'
+              mount "#{version.camelize}::#{resource_name.camelize}::#{action_file[0..-4].camelize}".constantize => '/'
+            end
+          end
+        end
+      end
+    end
+  end
+
+  mount GrapeSwaggerRails::Engine => '/docs'
 end
